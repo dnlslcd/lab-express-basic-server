fe1649071b79e8e0cc4bd1e3189b04590255b344
dnlslcd <danielsalcedoq@gmail.com>
2024-07-01 19:29:38 +0200

    Iteraciones 1 y 2 - hechas

--- app.js
+++ app.js
@@ -1,18 +1,21 @@
 // IMPORT PACKAGES
 // Here you should import the required packages for your Express app: `express` and `morgan`
-
+const express = require('express');
+const morgan = require('morgan');
+const logger = require('morgan');
 
 
 // CREATE EXPRESS APP
 // Here you should create your Express app:
+const app = express();
 
 
 
 // MIDDLEWARE
 // Here you should set up the required middleware:
-// - `express.static()` to serve static files from the `public` folder
-// - `express.json()` to parse incoming requests with JSON payloads
-// - `morgan` logger to log all incoming requests
+app.use(express.static('public')); // to serve static files from the `public` folder
+app.use(express.json()); // to parse incoming requests with JSON payloads
+app.use(morgan('dev')); // 'morgan' logger to log all incoming requests
 
 
 
@@ -23,3 +26,6 @@
 
 // START THE SERVER
 // Make your Express server listen on port 5005:
+app.listen(5005, ()=>{
+    console.log("Servidor corriendo en el puerto 5005.");
+});
